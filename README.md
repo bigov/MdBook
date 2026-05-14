6fefaa024db4c920736b30bc189d35bf7fe491e0
Bigov <bigov@yandex.com>
2026-05-14 11:08:13 +0900

    dev app config

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Markdown Book
+# Hyper-Markdown Book
 
-The Markdown Book is the WYSIWYG manager/editor for Markdown files. This application is open-source,  written in С++ using the wxWidgets library.
+The HM Book is the WYSIWYG manager/editor for Markdown files. This application is open-source,  written in С++ using the wxWidgets library.
 
 License: MIT
--- src/main.cpp
+++ src/main.cpp
@@ -23,13 +23,19 @@ bool MyApp::OnInit()
     int screenHeight = wxSystemSettings::GetMetric(wxSYS_SCREEN_Y);
 
     wxConfig config("Book", "Hyper-Markdown");
-    // Чтения с указанными значениями по умолчанию
+
+    /* Методы класса wxConfig:  ReadLong, ReadBool, ReadDouble и общего Read для wxString.
+      wxString name;
+      config.Read("/User/Name", &name, "");
+      double volume = config.ReadDouble("/User/Volume", 1.0);
+      bool maximized = config.ReadBool("/MainWindow/Maximized", false);
+     */
+
+    // Чтение со значениями по умолчанию
     int win_width  = (int)config.ReadLong("/MainWindow/Width", 800L);
     int win_height = (int)config.ReadLong("/MainWindow/Height", 800L);
-
     int win_x = (int)config.ReadLong("/MainWindow/X", 10L);
     int win_y = (int)config.ReadLong("/MainWindow/Y", 10L);
-    
     
     AppFrame* frame = new AppFrame("Hyper Markdown Book", win_x, win_y, win_width, win_height);
     frame->Show(true);
